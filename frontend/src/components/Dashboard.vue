12702e0cfad06067ea4bd4e1bc68f22d71e6f358
mhachey <michael.g.hachey@gmail.com>
2018-06-10 18:58:37 -0400

    added Dashboard to frontent router logic, removed routing logic to some sample app pages

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dashboard">
+      This is where the dashboard goes...
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'dashboard'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Service from '@/components/Service'
-import Bootstrap from '@/components/Bootstrap'
-import User from '@/components/User'
+import Hello from '../components/Hello'
+import Service from '../components/Service'
+import Bootstrap from '../components/Bootstrap'
+import User from '../components/User'
+import Dashboard from '../components/Dashboard'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/user',
       name: 'User',
       component: User
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
