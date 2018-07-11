680c8db5d2e86289e87f22c0c8b2afce190182e1
Mike Hachey <michael.g.hachey@gmail.com>
2018-07-11 12:37:42 -0400

    added timestamps to chat

---
+++ frontend/src/components/Chat/AlertPicker.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<div class="alert_picker">
+
+</div>
+
+<script>
+    export default {
+        name: "AlertPicker"
+    }
+</script>
+
+<style scoped>
+  div.alert_picker {
+  }
+
+</style>
--- frontend/src/components/Chat/Message.vue
+++ frontend/src/components/Chat/Message.vue
@@ -8,6 +8,9 @@
         <div v-html="message.content"></div>
         <chat-image v-if="message.image" :imgsrc="message.image" @imageLoad="imageLoad"></chat-image>
       </div>
+      <div class="timestamp">
+        {{ timestamp(message.date) }}
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +36,21 @@
     methods: {
       imageLoad () {
         this.$emit('imageLoad')
+      },
+      timestamp (moment) {
+        const message_date = new Date(moment);
+        const message_year = message_date.getFullYear();
+        const message_month = message_date.getMonth() + 1;
+        const message_day = message_date.getDate();
+        const message_hour = (message_date.getHours() > 12) ? message_date.getHours() - 12  : message_date.getHours();
+        const message_minutes = message_date.getMinutes();
+        const meridian = (message_date.getHours() > 11) ? 'PM' : 'AM';
+
+        const date_part = `${message_month}/${message_day}/${message_year}`;
+        const time_part =
+          `${message_hour}:${message_minutes.toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping:false})}`;
+
+        return `${date_part} ${time_part} ${meridian}`;
       }
     }
   }
@@ -43,5 +61,9 @@
     font-size: 20px;
     vertical-align: middle;
     line-height: 2;
+  }
+
+  div.timestamp {
+    font-size: smaller;
   }
 </style>
