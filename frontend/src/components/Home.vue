b7f3c00a5b920f886ecfa20e95aa4cc728c1da5b
Miffy Chen <mchen15@bu.edu>
2018-07-24 00:56:09 -0400

    Session cookie implementation (need help)
    All changes commented with "cookie" or "question" for easy search. Any help would be much appreciated.
    
    I was trying to add this on to our project, but not sure how it works.
    https://firebase.google.com/docs/auth/admin/manage-cookies
    
    So then I found this, npm-cookie, and have imported it, but I'm not sure where I need to establish the cookie. https://www.npmjs.com/package/vue-cookies
    
    I tried setting the cookie @signup, and then on all the pages just to try it out. But Vue only allows one <script> in each Vue component, so I'm having trouble enabling it.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -22,3 +22,7 @@
     }
   }
 </script>
+
+<!-- Question: only one script is allowed in Vue, how do I concat multiple scripts together??
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-cookies@1.5.5/vue-cookies.js"></script> -->
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,10 +9,15 @@ import { store } from './store'
 import AlertComponent from './components/Shared/Alert.vue'
 import { TableComponent, TableColumn } from 'vue-table-component'
 
+
 require('../node_modules/vue-table-component/docs/table-component.css')
 
 Vue.use(Vuetify)
 Vue.use(VueResource)
+
+// cookies
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
 
 Vue.config.productionTip = false
 
@@ -33,6 +38,30 @@ new Vue({
       databaseURL: 'https://trio-cs673-firebase.firebaseio.com',
       projectId: 'trio-cs673-firebase',
       storageBucket: 'trio-cs673-firebase.appspot.com'
-    })
+    });
+
+
+    // Question: not sure how these work... pulled from Firebase documentation
+
+    // As httpOnly cookies are to be used, do not persist any state client side.
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+
+    // When the user signs in with email and password.
+    firebase.auth().signInWithEmailAndPassword('mchen15@bu.edu', 'password').then(user => {
+      // Get the user's ID token as it is needed to exchange for a session cookie.
+      return user.getIdToken().then(idToken => {
+        // Session login endpoint is queried and the session cookie is set.
+        // CSRF protection should be taken into account.
+        // ...
+        const csrfToken = getCookie('csrfToken')
+        return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
+      });
+    }).then(() => {
+      // A page redirect would suffice as the persistence is set to NONE.
+      return firebase.auth().signOut();
+    }).then(() => {
+      window.location.assign('/profile');
+    });
+
   }
 })
