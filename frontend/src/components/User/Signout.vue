0a81b4aa876649227bd0dcdf37437e29f8d1cc70
Miffy Chen <mchen15@bu.edu>
2018-07-27 20:07:25 -0400

    logout
    Added an extra logout page... with a button on it, that sucessfully logs out the user when clicked.
    
    What is NOT solved:
    'login' button on the login page still shows as loading after logging out and being redirected back to the login page

---
+++ frontend/src/components/User/Signout.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="onSignOut">
+  <v-btn type="submit">Logout</v-btn>
+  </form>
+</template>
+
+<script>
+  export default {
+    computed: {
+      user () {
+        return this.$store.getters.user
+      }
+    },
+    watch: {
+      user (value) {
+        if (value == null || value == undefined) {
+          this.$router.push('/')
+        }
+      }
+    },
+    methods: {
+      onSignOut () {
+        this.$store.dispatch('signUserOut')
+      }
+    }
+  }
+</script>
--- frontend/src/store/AuthModule.js
+++ frontend/src/store/AuthModule.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase'
+
 const AuthModule = {
   state: {
     user: null
@@ -35,8 +36,7 @@ const AuthModule = {
       commit('setLoading', true)
       commit('clearError')
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
+        .then(user => {
             firebase.database().ref('users').child(user.uid).set({
               fullname: payload.fullname,
               firstname: payload.firstname,
@@ -45,20 +45,18 @@ const AuthModule = {
               id: user.uid,
               rooms: []
             })
-              .then(
-                message => {
+              .then(message => {
                   commit('setLoading', false)
                   const newUser = {
                     id: user.uid,
                     fullname: payload.fullname
                   }
                   commit('setUser', newUser)
-                }
-              )
+                })
               .catch(
                 error => {
                   commit('setLoading', false)
-                  commit('setError', error)
+                  commit('setError', error.message)
                 }
               )
           }
@@ -74,22 +72,34 @@ const AuthModule = {
       commit('setLoading', true)
       commit('clearError')
       firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            firebase.database().ref('users').child(user.uid).once('value', function (data) {
-              commit('setLoading', false);
-              const newUser = {
-                id: user.uid,
-                fullname: data.val().fullname,
-                firstname: data.val().firstname,
-                lastname: data.val().lastname,
-                email: data.val().email,
-                rooms: data.val().rooms
-              };
-              commit('setUser', newUser)
-            })
+        .then(user => {
+          firebase.database().ref('users').child(user.uid).once('value', function (data) {
+            commit('setLoading', false);
+            const newUser = {
+              id: user.uid,
+              fullname: data.val().fullname,
+              firstname: data.val().firstname,
+              lastname: data.val().lastname,
+              email: data.val().email,
+              rooms: data.val().rooms
+            };
+            commit('setUser', newUser)
+          })
+        })
+        .catch(
+          error => {
+            commit('setLoading', false)
+            commit('setError', error)
           }
         )
+    },
+    signUserOut ({commit}) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().signOut()
+        .then(user => {
+          commit('setUser', null)
+        })
         .catch(
           error => {
             commit('setLoading', false)
@@ -112,8 +122,7 @@ const AuthModule = {
             rooms: user.rooms
           };
           commit('setUserOnly', newUser)
-        }
-        )
+        })
         .catch(
           error => {
             commit('setLoading', false)
